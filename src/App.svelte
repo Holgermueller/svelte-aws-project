4a62ea0fc076c85b2cd89965fd59ffa5f25e55fb
Holger Mueller <9jholger@gmail.com>
2021-08-23 13:29:22 -0500

    added header

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,10 @@
 <script>
+  import Header from "./layout/Header.svelte";
   import Footer from "./layout/Footer.svelte";
 </script>
+
+<Header />
+
+<h1>Things will go here!!</h1>
 
 <Footer />
---
+++ src/layout/Header.svelte
@@ -0,0 +1,20 @@
+<header>
+  <h1>Header</h1>
+</header>
+
+<style>
+  header {
+    background: #f7f7f7;
+    padding: 20px;
+    top: 0;
+    width: 100%;
+  }
+  h1 {
+    margin: 0;
+    text-align: center;
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+</style>
