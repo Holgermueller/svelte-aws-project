c2b765437913e5c5180885dacc97f22c6e444e48
Holger Mueller <9jholger@gmail.com>
2021-08-22 19:31:17 -0500

    added footer

---
+++ src/layout/Footer.svelte
@@ -0,0 +1,19 @@
+<footer>
+  <div class="copyright">&copy; 2021 Holger Mueller</div>
+</footer>
+
+<style>
+  footer {
+    background: #f7f7f7;
+    padding: 5px;
+    bottom: 0;
+    position: relative;
+    width: 100%;
+  }
+  div {
+    margin: 0;
+    text-align: center;
+    color: #ff3e00;
+    text-transform: uppercase;
+  }
+</style>
