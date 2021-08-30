ed8b7e7990433353eeff2acd18fef027f0cb7522
Holger Mueller <9jholger@gmail.com>
2021-08-30 13:24:06 -0500

    began scaffold form

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
   import CurrentlyReading from "./Components/CurrentlyReading.svelte";
   import Modal from "./shared/Modal.svelte";
   import Button from "./shared/Button.svelte";
+  import AddBook from "./Components/AddBook.svelte";
   import BooksRead from "./Components/BooksRead.svelte";
   import Footer from "./layout/Footer.svelte";
 
@@ -18,7 +19,7 @@
 <CurrentlyReading />
 
 <Modal {showModal} on:click={toggleModal}>
-  <h1>This is the modal</h1>
+  <AddBook />
 </Modal>
 
 <Button on:click={toggleModal}>Add A Book</Button>
